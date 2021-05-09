32e9f80ff7a80e3e452f4380922d034c37ce4b0e
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2021-05-09 14:39:20 +0500

    Create detect.py
    Requirements:
    
    Python3

---
+++ detect.py
@@ -0,0 +1,55 @@
+import os
+import json
+import urllib.request as urllib2
+import sys
+
+
+sys.ps1 = '\033[94m'
+
+
+print(sys.ps1)
+
+os.system('clear || cls')
+
+
+print('''
+
+███████╗██╗   ██╗██╗██╗     
+██╔════╝██║   ██║██║██║     
+█████╗  ██║   ██║██║██║     
+██╔══╝  ╚██╗ ██╔╝██║██║     
+███████╗ ╚████╔╝ ██║███████╗
+╚══════╝  ╚═══╝  ╚═╝╚══════╝
+                            
+
+Made By V1 MoDzZz
+
+''')
+
+def hello():
+    ip = input("""┌─[v1modzzz@IP:~# 
+└──╼Enter IP Address: """)
+    url = f"https://ipqualityscore.com/api/json/ip/EjOM79qU69INQWrjJahbPiS51wGZUBfn/{ip}?strictness=0&allow_public_access_points=true&fast=true&lighter_penalties=true&mobile=true"
+    response = urllib2.urlopen(url)
+    data = response.read()
+    values = json.loads(data)
+    
+    
+    print("Host: ", end="")
+    print(values['host'])
+    print("Proxy: ", end="")
+    print(values['proxy'])
+    print("VPN: ", end="")
+    print(values['vpn'])
+    print("Tor: ", end="")
+    print(values['tor'])
+    print("Active VPN: ", end="")
+    print(values['active_vpn'])
+    print("Active Tor: ", end="")
+    print(values['active_tor'])
+    print("Bot Status: ", end="")
+    print(values['bot_status'])
+    
+    os.system('pause')
+    
+hello()
